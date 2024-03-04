8230521c0af81b9341f14710bea43eb2814278aa
vgarzareyna <vgarzareyna@gmail.com>
2024-03-03 20:57:20 -0600

    Added LICENSE and README.md Changed Icon
    Updated `make.sh`

---
+++ README.md
@@ -0,0 +1,5 @@
+# D20PFSRD Search
+
+This extension was created to make easier searching for pages inside [D20PFSRD](https://www.d20pfsrd.com)
+
+This project is in no way linked to [d20pfsrd.com](https://www.d20pfsrd.com)
--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@ echo "Starting"
 if [ ! -d "release" ]; then
 	mkdir release
 fi
-zip -r -FS ./release/firefox.zip ./manifest.json ./icons
+zip -r -FS ./release/firefox.zip ./manifest.json ./icons/icon-48.png
 
 echo "Finished"
 	
